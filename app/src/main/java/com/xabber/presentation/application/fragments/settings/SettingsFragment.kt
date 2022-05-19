70050426db2f6d8a19cf6861bdc27ba7cc660e2d
olesya.garipova <garipolesya@mail.ru>
2022-05-19 18:16:00 +0500

    switch to develop

--- app/src/main/java/com/xabber/presentation/application/fragments/settings/SettingsFragment.kt
+++ app/src/main/java/com/xabber/presentation/application/fragments/settings/SettingsFragment.kt
@@ -2,6 +2,7 @@ package com.xabber.presentation.application.fragments.settings
 
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatDelegate
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.xabber.R
 import com.xabber.databinding.FragmentSettingsBinding
@@ -10,10 +11,16 @@ import com.xabber.presentation.application.fragments.BaseFragment
 
 class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
     private val binding by viewBinding(FragmentSettingsBinding::bind)
+    private var isDarkMode = false
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.imMode.setOnClickListener {
+            isDarkMode = !isDarkMode
+            if (isDarkMode) AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+           else  AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
+        }
 
         binding.linearChat.setOnClickListener {
             navigator().showChatSettings()
