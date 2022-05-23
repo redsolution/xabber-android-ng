ad459b8a83f7f06554a09e193af03b6d17346348
Igor Boldin <igor.boldin@redsolution.com>
2022-05-23 12:26:25 +0500

    fixes db

--- app/src/main/java/com/xabber/xmpp/roster/RosterStorageItem.kt
+++ app/src/main/java/com/xabber/xmpp/roster/RosterStorageItem.kt
@@ -1,9 +1,14 @@
 package com.xabber.xmpp.roster
 
+import com.xabber.defaultRealmConfig
 import com.xabber.utils.storage.prp
 import com.xabber.xmpp.avatar.AvatarStorageItem
+import com.xabber.xmpp.presences.ResourceStorageItem
+import io.realm.Realm
 import io.realm.RealmObject
+import io.realm.RealmResults
 import io.realm.annotations.PrimaryKey
+import io.realm.query
 
 class RosterStorageItem: RealmObject {
     @PrimaryKey
@@ -31,6 +36,28 @@ class RosterStorageItem: RealmObject {
     var subscribtion: RosterSubscribtion
         get() = RosterSubscribtion.values().firstOrNull { it.rawValue == subscribtion_ } ?: RosterSubscribtion.Undefined
         set(newValue: RosterSubscribtion) { subscribtion_ = newValue.rawValue }
+
+    var primaryResource: ResourceStorageItem?
+        get() {
+            val realm = Realm.open(configuration = defaultRealmConfig())
+            val resources: RealmResults<ResourceStorageItem> = realm
+                .query<ResourceStorageItem>("owner == $owner AND jid == $jid AND SORT(timestamp DESC) AND SORT(priority DESC)")
+                .find()
+            return resources.firstOrNull()
+        }
+        private set(newValue: ResourceStorageItem?) { error("you cant set primary resource") }
+
+    var displayName: String
+        get() {
+            if (customNickname.trim().length > 0) {
+                return customNickname.trim()
+            }
+            if (nickname.length > 0) {
+                return nickname
+            }
+            return jid
+        }
+        set(newValue: String) { error("you cant set display name") }
 
     companion object {
         fun genPrimary(jid: String, owner: String): String {
